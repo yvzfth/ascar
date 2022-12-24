9731626b69d8537a97a99daa1f1dd670234c7641
Fatih Yavuz <yvzfth@yandex.com>
2022-12-24 23:20:10 +0300

    car component undefined error fix 8

--- pages/cars/[id]/index.tsx
+++ pages/cars/[id]/index.tsx
@@ -23,83 +23,84 @@ const Car = () => {
   const car = getCars().find((car) => car.id === Number(id))!;
 
   const [logedIn, setLogedIn] = React.useState<true | false>(false);
+  if (car !== undefined)
+    return (
+      <div>
+        <Head>
+          <title>AsCar</title>
+          <meta name='description' content='AsCar - Araç Kirala, Al, Sat' />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <Navbar />
+        <div className='container mx-auto py-4'>
+          {/* <h1 className='text-3xl font-bold text-center mb-4'>Renting Page</h1> */}
 
-  return (
-    <div>
-      <Head>
-        <title>AsCar</title>
-        <meta name='description' content='AsCar - Araç Kirala, Al, Sat' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <Navbar />
-      <div className='container mx-auto py-4'>
-        {/* <h1 className='text-3xl font-bold text-center mb-4'>Renting Page</h1> */}
-
-        <div>
-          <Header
-            make={car!?.make}
-            model={car!?.model}
-            year={car!?.year}
-            fuelType={car!?.fuelType}
-            transmission={car!?.transmission}
-          />
-          <div className='mb-4'>
-            <CarImageSlider
-              imageUrls={['/car1.jpeg', '/car2.jpeg', '/car3.jpeg']}
+          <div>
+            <Header
+              make={car!?.make}
+              model={car!?.model}
+              year={car!?.year}
+              fuelType={car!?.fuelType}
+              transmission={car!?.transmission}
             />
-          </div>
-          <div className='py-4'>
-            <div className='text-red-500 text-3xl font-extrabold'>
-              {car!?.price}₺
+            <div className='mb-4'>
+              <CarImageSlider
+                imageUrls={['/car1.jpeg', '/car2.jpeg', '/car3.jpeg']}
+              />
             </div>
-            <div>{logedIn ? '⭐️' : '☆'} Favorilerine ekle</div>
-          </div>
-          <div className='bg-white rounded-lg shadow-md p-4 dark:text-black'>
-            <h2 className='text-xl font-bold mb-2'>Engine</h2>
-            <p>{car!?.engine}</p>
-            <h2 className='text-xl font-bold mt-2 mb-2'>Transmission</h2>
-            <p>{car!?.transmission}</p>
-            <h2 className='text-xl font-bold mt-2 mb-2'>Fuel Type</h2>
-            <p>{car!?.fuelType}</p>
-            <h2 className='text-xl font-bold mt-2 mb-2'>Color</h2>
-            <p>{car!?.color}</p>
-            <h2 className='text-xl font-bold mt-2 mb-2'>Mileage</h2>
-            <p>{car!?.mileage}</p>
-            <h2 className='text-xl font-bold mt-2 mb-2'>Price</h2>
-            <p>${car!?.price}</p>
-          </div>
-          <Technologies
-            technologies={car!?.technologies}
-            bgImageUrl={car!?.imageUrl}
-          />
-          <Features features={car!?.features} />
+            <div className='py-4'>
+              <div className='text-red-500 text-3xl font-extrabold'>
+                {car!?.price}₺
+              </div>
+              <div>{logedIn ? '⭐️' : '☆'} Favorilerine ekle</div>
+            </div>
+            <div className='bg-white rounded-lg shadow-md p-4 dark:text-black'>
+              <h2 className='text-xl font-bold mb-2'>Engine</h2>
+              <p>{car!?.engine}</p>
+              <h2 className='text-xl font-bold mt-2 mb-2'>Transmission</h2>
+              <p>{car!?.transmission}</p>
+              <h2 className='text-xl font-bold mt-2 mb-2'>Fuel Type</h2>
+              <p>{car!?.fuelType}</p>
+              <h2 className='text-xl font-bold mt-2 mb-2'>Color</h2>
+              <p>{car!?.color}</p>
+              <h2 className='text-xl font-bold mt-2 mb-2'>Mileage</h2>
+              <p>{car!?.mileage}</p>
+              <h2 className='text-xl font-bold mt-2 mb-2'>Price</h2>
+              <p>${car!?.price}</p>
+            </div>
+            <Technologies
+              technologies={car!?.technologies}
+              bgImageUrl={car!?.imageUrl}
+            />
+            <Features features={car!?.features} />
 
-          <div className='w-full h-[30rem]'>
-            <MapContainer />
+            <div className='w-full h-[30rem]'>
+              <MapContainer />
+            </div>
           </div>
-        </div>
 
-        <WhatsAppChatButton />
-        <Button
-          className='w-full'
-          variant='contained'
-          color='error'
-          href='#SendEmail'
-        >
-          Mesaj Gönder
-        </Button>
-        <RelatedCars id={String(id!)} />
-        <div className='h-screen'></div>
-        <div id='SendEmail'></div>
-        <div className='flex w-full items-center justify-center'>
-          <SendEmail />
+          <WhatsAppChatButton />
+          <Button
+            className='w-full'
+            variant='contained'
+            color='error'
+            href='#SendEmail'
+          >
+            Mesaj Gönder
+          </Button>
+          <RelatedCars id={String(id!)} />
+          <div className='h-screen'></div>
+          <div id='SendEmail'></div>
+          <div className='flex w-full items-center justify-center'>
+            <SendEmail />
+          </div>
+          <div className='h-screen'></div>
         </div>
-        <div className='h-screen'></div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  else return <div></div>;
 };
 
 export default Car;
